cc2ba702ff13db94c9b7d0d86b4493410f6bc97a
Akash Dathan <akashdathan@gmail.com>
2023-08-23 20:46:17 +0200

    :recycle: alignment fix

--- pages/blog.js
+++ pages/blog.js
@@ -20,11 +20,11 @@ export default function Blog({ preview, allPosts }) {
 
                   <div className="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
                       {allPosts.map((post) => (
-                          <article className="flex max-w-xl flex-col items-start justify-between">
+                          <article className="flex max-w-xl flex-col items-start">
                               <a href={`/blog/${post.slug}`}>
                                   <img className="rounded-xl" src={post.coverImage?.url} alt={post.title}/>
                               </a>
-                              <div className="flex items-center gap-x-4 text-xs">
+                              <div className="flex items-start gap-x-4 text-xs">
                                   <time dateTime={post.date} className="text-gray-500 mt-2">
                                       {new Date(post.date).toDateString()}
                                   </time>

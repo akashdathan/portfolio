a67f857dca618618bb0a04ed188b31c431eeb345
Akash Dathan <akashdathan@gmail.com>
2023-08-27 23:11:50 +0200

    :recycle: css tweak

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -13,8 +13,8 @@ export default async  function Blog() {
 		<div className="bg-white py-24 sm:py-32">
 			<div className="mx-auto max-w-7xl px-6 lg:px-8">
 				<div className="mx-auto max-w-2xl lg:mx-0">
-					<h2 className="text-5xl font-bold tracking-tight text-gray-900">From the blog</h2>
-					<p className="mt-2 text-xl leading-8 text-gray-600">
+					<h2 className="text-5xl lg:text-6xl font-bold tracking-tight text-gray-900">From the blog</h2>
+					<p className="text-lg lg:text-xl mt-2 leading-8 text-gray-600">
 						Here, I unravel the intricacies of software development, I use dev.to for writing and publishing, here is a list of what I have published.
 					</p>
 				</div>

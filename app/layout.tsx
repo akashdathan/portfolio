1c4bb98d95a41e985d12d4f791c1f88927bc8cf1
Akash Dathan <akashdathan@gmail.com>
2023-08-27 22:52:45 +0200

    :bug: manifest load issue fixed

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import './globals.css';
-import Head from 'next/head';
 import { Inter } from 'next/font/google';
 import { usePathname } from 'next/navigation';
 import Header from './components/Header';
@@ -18,12 +17,10 @@ export default function RootLayout({
 
 	return (
 		<html lang="en">
-			<Head>
-				<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
-				<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
-				<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
-				<link rel="manifest" href="/site.webmanifest"/>
-			</Head>
+			{/*Need this for PWA, Next13 not yet supports this*/}
+			<head>
+				<meta name="theme-color" content="#0000FF" />
+			</head>
 			<body className={inter.className}>
 				<div className="bg-white flex flex-col min-h-screen">
 					<Header/>
